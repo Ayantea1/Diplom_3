5ddbb803abf73f8c1fdee984e39a003b442fc453
Ekaterina Mukhacheva <ayantea@yandex.com>
2023-01-20 12:06:03 +0500

    fix pom

--- src/main/java/POM/PageMethod.java
+++ src/main/java/POM/PageMethod.java
@@ -1,11 +1,7 @@
 package POM;
 
-import org.hamcrest.MatcherAssert;
-import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,13 +11,9 @@ import java.util.concurrent.TimeUnit;
 public class PageMethod {
 
     public WebDriver driver;
-  //  public WebDriverWait wait;
-
-  //  public static final int TIME_OUT_IN_SECONDS_FOR_FIND_WEB_ELEMENT = 10;
 
     public PageMethod(WebDriver driver) {
         this.driver = driver;
-      //  wait = new WebDriverWait(driver, Duration.ofSeconds(TIME_OUT_IN_SECONDS_FOR_FIND_WEB_ELEMENT));
     }
 
     public void openPage(String url){
